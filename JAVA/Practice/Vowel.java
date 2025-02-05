f3ec4e44b7a556feefa828266c6e84afb3d34c0f
Aneek <iamaneek.debnath@gmail.com>
2025-02-06 01:20:29 +0530

    s

--- JAVA/Practice/Vowel.java
+++ JAVA/Practice/Vowel.java
@@ -2,19 +2,20 @@ import java.util.*;
 
 class Vowel {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a, b, v = 0;
-        String st;
-        char chr;
-        System.out.println("Enter the string: ");
-        st = sc.nextLine();
-        b = st.length();
-        for (a = 0; a < b; a++) {
-            chr = st.charAt(a);
-            if(chr == 'A' || chr == 'E' || chr == 'I' || chr == 'O' || chr == 'U' || chr == 'a' || chr == 'e' || chr == 'i' || chr == 'o' || chr == 'u') {
-                v++;
+       try (Scanner sc = new Scanner(System.in)) {
+            int a, b, v = 0;
+            String st;
+            char chr;
+            System.out.println("Enter the string: ");
+            st = sc.nextLine();
+            b = st.length();
+            for (a = 0; a < b; a++) {
+                chr = st.charAt(a);
+                if(chr == 'A' || chr == 'E' || chr == 'I' || chr == 'O' || chr == 'U' || chr == 'a' || chr == 'e' || chr == 'i' || chr == 'o' || chr == 'u') {
+                    v++;
+                }
             }
+                System.out.println("Number of vowels in the string is: " + v);
         }
-        System.out.println("Number of vowels in the string is: " + v);
     }
 }
